1d78d1edc8e3b11511035b785f029e470ff637fc
Filip-Gheorghe Caracoancea <filip.caracoancea@emanuel.ro>
2024-01-13 17:30:19 +0100

    tema

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine();
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int suma = a + b;
+
+        Console.WriteLine(suma);
+    }
+}
